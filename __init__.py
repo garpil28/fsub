104948efc9c521d3c07a01c89c2af6f475084353
garpil28 <storegarfield076@gmail.com>
2025-11-04 04:39:18 +0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# __init__.py
+"""
+AutopostPro Bot Framework
+==========================
+Versi profesional bot FSUB multi-owner dengan sistem lisensi dan pembayaran otomatis.
+
+Modules:
+- handlers   : command & event handler
+- utils      : database & lisensi
+- services   : pembayaran dan validasi
+"""
